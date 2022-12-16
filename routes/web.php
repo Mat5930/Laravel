b012f2ae444f0f98327953a135de912bf81032ed
[Masquelier] <[mathis.masquelier@gmail.com]>
2022-12-16 09:30:43 +0100

    création d'une route et modif template

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,15 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-//Route::get('/hello', function () {
-  //  return view('hello');
-//})->name('hello');
+Route::get('/hello/{name}', function ($name)  {
+    //traitement des données
+        $name = '"'.$name.'"';
+    
+    return view('hello',  [
+    //passage d'une variable a une vue 
+    'name' => $name,
+  ]);
+})->name('hello');
 
 Route::get('/menu', function () {
     return view('menu');
